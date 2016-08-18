882a8a05e65a52733f7c0fdfe2512f326dfc175c
HCI01\meads <meads@hcpci.com>
2016-08-18 14:02:51 -0400

    delete items

--- app/components/GroceryItem.jsx
+++ app/components/GroceryItem.jsx
@@ -1,10 +1,19 @@
 var React = require('react/addons');
+var action = require('./../actions/GroceryItemActionCreator.jsx');
 
 module.exports = React.createClass({
+    delete: function(e) { 
+        e.preventDefault();
+        action.delete(this.props.item);
+    },
     render:function(){
         return (
+
             <li className={this.props.item.purchased ? "strikethrough" : null}>
-                {this.props.item.name}
+                <div>{this.props.item.name}</div>
+                <form className="three columns" onSubmit={this.delete}>
+                    <button>&times;</button>
+                </form>
             </li>
         )
     }
--- app/stores/GroceryItemStore.jsx
+++ app/stores/GroceryItemStore.jsx
@@ -32,12 +32,30 @@ function GroceryItemStore() {
         triggerListeners();
     }
 
+      function deleteGroceryItem(item) {
+        
+        var index;
+        
+        items.filter(function(_item, _index) { 
+            if(_item.name === item.name) {
+                index = _index;
+                return;
+            }
+        });
+
+        items.splice(index, 1);
+        triggerListeners();
+    }
+
     dispatcher.register(function(event) { 
         var split = event.type.split(':');
         if(split[0] === 'grocery-item') {
             switch(split[1]){
                 case 'add':
                     addGroceryItem(event.payload); 
+                break;
+                 case 'delete':
+                    deleteGroceryItem(event.payload); 
                 break;
                 
             }
